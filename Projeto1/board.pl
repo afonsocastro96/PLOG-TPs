bc52692ed1afa1cf65476059c43200d77b44d6db
Sk0ut <poke_flavio@hotmail.com>
2015-10-08 11:31:36 +0100

    Drawing OP, no hardcoded length

--- Projeto1/board.pl
+++ Projeto1/board.pl
@@ -1,24 +1,26 @@
-dummy_board(Board) :- Board is 
-[
-[[' ',' ',' '], [' ',' ',' '], [' ','B','C'], [' ','B','Q'], [' ','B','Q'], [' ',' ',' '],[' ',' ',' ']],
-[[' ',' ',' '], [' ','P','Q'], [' ','P','C'], [' ','P','Q'], [' ','B','C'], [' ','P','C'],[' ',' ',' ']],
-[[' ','P','C'], [' ','B','Q'], [' ','B','C'], [' ','B','Q'], [' ','P','Q'], [' ','B','C'],[' ','B','C']],
-[[' ','P','Q'], [' ','P','C'], [' ','P','C'], [' ',' ',' '], [' ','B','Q'], [' ','B','Q'],[' ','B','C']],
-[[' ','P','Q'], [' ','P','Q'], [' ','B','C'], [' ','P','C'], [' ','P','Q'], [' ','P','C'],[' ','B','Q']],
-[[' ',' ',' '], [' ','B','Q'], [' ','P','Q'], [' ','B','C'], [' ','B','Q'], [' ','B','C'],[' ',' ',' ']],
-[[' ',' ',' '], [' ',' ',' '], [' ','P','C'], [' ','P','C'], [' ','P','Q'], [' ',' ',' '],[' ',' ',' ']],
-].
+dummy_board(Board) :- Board =
+[[[' ',' ',' '], [' ',' ',' '], [' ','B','C'], [' ','B','Q'], [' ','B','Q'], [' ',' ',' '], [' ',' ',' ']],
+ [[' ',' ',' '], [' ','P','Q'], [' ','P','C'], [' ','P','Q'], ['L','B','C'], [' ','P','C'], [' ',' ',' ']],
+ [[' ','P','C'], [' ','B','Q'], ['L','B','C'], [' ','B','Q'], [' ','P','Q'], [' ','B','C'], [' ','B','C']],
+ [[' ','P','Q'], [' ','P','C'], [' ','P','C'], [' ',' ',' '], ['T','B','Q'], [' ','B','Q'], [' ','B','C']],
+ [[' ','P','Q'], [' ','P','Q'], [' ','B','C'], [' ','P','C'], [' ','P','Q'], [' ','P','C'], [' ','B','Q']],
+ [[' ',' ',' '], ['T','B','Q'], [' ','P','Q'], [' ','B','C'], [' ','B','Q'], [' ','B','C'], [' ',' ',' ']],
+ [[' ',' ',' '], [' ',' ',' '], [' ','P','C'], [' ','P','C'], [' ','P','Q'], [' ',' ',' '], [' ',' ',' ']]].
 
-display_board(Board) :- display_board_aux(Board,7).
+display_board(Board) :- display_board_aux(Board).
 
-display_board_aux(Board,0) :- write_border(7).
+display_board_aux([Line |Board]) :- write_border(Line), write_line(Line), display_board_aux(Board).
 
-display_board_aux(Board,N) :- N > 0, write_border(7), write_line(Board,7), Y is N-1, display_board_aux(Board,Y).
+display_board_aux([Line |[]]) :- write_border(Line), write_line(Line), write_border(Line).
 
-write_line(Board,0) :- write('|\n').
+write_border([]) :- write('+\n').
 
-write_line(Board,N) :- N > 0, write('|   '), Y is N-1, write_line(Board,Y).
+write_border([Elem|Line]) :- write('+---'), write_border(Line).
 
-write_border(0) :- write('+\n').
+write_line([]) :- write('|\n').
 
-write_border(N) :- N > 0, write('+---'), Y is N-1, write_border(Y).
+write_line([Elem|Line]) :- write('|'), write_elem(Elem), write_line(Line).
+
+write_elem([Elem1|Elems]) :- write(Elem1), write_elem(Elems).
+
+write_elem([]).
