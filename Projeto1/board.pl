691b30c4248a7c381d08f5338fbc52d8a477548a
Sk0ut <poke_flavio@hotmail.com>
2015-10-09 17:54:51 +0100

    Fix small redundancy that made the code order dependent.

--- Projeto1/board.pl
+++ Projeto1/board.pl
@@ -1,4 +1,13 @@
-dummy_board(Board) :- Board =
+major_board(Board) :- Board =
+[[[' ',' ',' '], [' ',' ',' '], [' ','B','C'], [' ','B','Q'], [' ','B','Q'], [' ',' ',' '], [' ',' ',' ']],
+ [[' ',' ',' '], [' ','P','Q'], [' ','P','C'], [' ','P','Q'], ['L','B','C'], [' ','P','C'], [' ',' ',' ']],
+ [[' ','P','C'], [' ','B','Q'], ['L','B','C'], [' ','B','Q'], [' ','P','Q'], [' ','B','C'], [' ','B','C']],
+ [[' ','P','Q'], [' ','P','C'], [' ','P','C'], [' ',' ',' '], ['T','B','Q'], [' ','B','Q'], [' ','B','C']],
+ [[' ','P','Q'], [' ','P','Q'], [' ','B','C'], [' ','P','C'], [' ','P','Q'], [' ','P','C'], [' ','B','Q']],
+ [[' ',' ',' '], ['T','B','Q'], [' ','P','Q'], [' ','B','C'], [' ','B','Q'], [' ','B','C'], [' ',' ',' ']],
+ [[' ',' ',' '], [' ',' ',' '], [' ','P','C'], [' ','P','C'], [' ','P','Q'], [' ',' ',' '], [' ',' ',' ']]].
+ 
+minor_board(Board) :- Board =
 [[[' ',' ',' '], [' ',' ',' '], [' ','B','C'], [' ','B','Q'], [' ','B','Q'], [' ',' ',' '], [' ',' ',' ']],
  [[' ',' ',' '], [' ','P','Q'], [' ','P','C'], [' ','P','Q'], ['L','B','C'], [' ','P','C'], [' ',' ',' ']],
  [[' ','P','C'], [' ','B','Q'], ['L','B','C'], [' ','B','Q'], [' ','P','Q'], [' ','B','C'], [' ','B','C']],
@@ -9,18 +18,16 @@ dummy_board(Board) :- Board =
 
 display_board(Board) :- display_board_aux(Board).
 
-display_board_aux([Line |Board]) :- write_border(Line), write_line(Line), display_board_aux(Board).
+display_board_aux([Line | Board]) :- Board \= [], write_border(Line), write_line(Line), display_board_aux(Board).
 
-display_board_aux([Line |[]]) :- write_border(Line), write_line(Line), write_border(Line).
+display_board_aux([Line]) :- write_border(Line), write_line(Line), write_border(Line).
 
 write_border([]) :- write('+\n').
 
-write_border([Elem|Line]) :- write('+---'), write_border(Line).
+write_border([_|Line]) :- write('+---'), write_border(Line).
 
 write_line([]) :- write('|\n').
 
 write_line([Elem|Line]) :- write('|'), write_elem(Elem), write_line(Line).
 
-write_elem([Elem1|Elems]) :- write(Elem1), write_elem(Elems).
-
-write_elem([]).
+write_elem([Tower,Colour,Shape]) :- write(Tower), write(Colour), write(Shape).
